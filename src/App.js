6dcd2b054080bbee17de1f01175056667c36763d
Brandon Lau <brandon.lau86@gmail.com>
2020-01-09 18:37:38 -0500

    add themecontext

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Booklist from './components/Booklist';
+import ThemeContextProvider from './contexts/ThemeContext';
 
 function App() {
   return (
     <div className="App">
-      <Booklist />
+      <ThemeContextProvider>
+        <Booklist />
+      </ThemeContextProvider>
     </div>
   );
 }
---
+++ src/contexts/ThemeContext.js
@@ -0,0 +1,20 @@
+import React, { Component, createContext } from 'react';
+
+export const ThemeContext = createContext();
+
+class ThemeContextProvider extends Component {
+  state = { 
+    LightTheme: true,
+    light: {bg: "#eed"},
+    dark: {bg: "444"}
+  }
+  render() { 
+    return ( 
+      <ThemeContext.Provider value={...this.state}>
+
+      </ThemeContext.Provider>
+    );
+  }
+}
+ 
+export default ThemeContextProvider;
